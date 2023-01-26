198c57f46b3dd404afddebea4c7fca5fe139c05b
Qasim Hussain <qasim.hussain1@kpmg.co.uk>
2023-01-26 10:19:47 +0000

    Image-Viewer

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -10,5 +10,5 @@ export default function ImageViewer({ placeholderImageSource }) {
 const styles = StyleSheet.create({
   image: { width: 320, 
           height: 440, 
-          borderRadius: 18 },
+        borderRadius: 18 },
 });
