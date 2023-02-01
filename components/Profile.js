4ad015babdf2f3cef61a37cc1abaae5a2cc99495
Neharik Umashankar <neharik.umashankar@kpmg.co.uk>
2023-02-01 16:52:48 +0000

    Styled some of the profile components

--- components/Profile.js
+++ components/Profile.js
@@ -1,84 +1,121 @@
-import { useContext } from "react"
-import { UserContext } from "./UserContext"
-import { AdultContext } from "./AdultContext"
-import { Image } from "react-native"
-import { View, Text } from "react-native"
-import Pressable from "react-native/Libraries/Components/Pressable/Pressable"
-import ImageViewer from "./ImageViewer"
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+import { AdultContext } from "./AdultContext";
+import { Image } from "react-native";
+import { View, Text, Pressable, ScrollView } from "react-native";
+// import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import ImageViewer from "./ImageViewer";
 
 function Profile({ navigation }) {
-    const { over18, setOver18 } = useContext(AdultContext)
-    const { loggedUser, setLoggedUser } = useContext(UserContext)
+  const { over18, setOver18 } = useContext(AdultContext);
+  const { loggedUser, setLoggedUser } = useContext(UserContext);
 
-    const handleGuestAgeChange = () => {
-        navigation.navigate("Welcome")
-    }
-    const handleGuestSignUp = () => {
-        navigation.navigate("Sign up")
-    }
+  const handleGuestAgeChange = () => {
+    navigation.navigate("Welcome");
+  };
 
-    const handleLogOut = () => {
-        setLoggedUser(false)
-        navigation.navigate("Welcome")
-    }
+  const handleLogin = () => {
+    navigation.navigate("Login");
+  };
+  const handleGuestSignUp = () => {
+    navigation.navigate("Sign up");
+  };
 
-    const handleEdit = () => {
-        navigation.navigate("Edit")
-    }
-    if (typeof loggedUser !== "object") {
+  const handleLogOut = () => {
+    setLoggedUser(false);
+    navigation.navigate("Welcome");
+  };
 
-        return (
-            <View className="flex-col items-center justify-center">
-                <Text>
-                    You are currently logged in as a guest who {over18 ? "is aged 18 or over." : "is not over age 18."} {"\n"}
-                </Text>
-                <Pressable onPress={handleGuestAgeChange}>
-                    <Text className="flex-col h-auto text-center items-center justify-center border-2 border-solid border-black rounded-xl">Click to change age bracket.{"\n"}</Text>
-                </Pressable>
-                <Pressable onPress={handleGuestSignUp}>
-                    <Text className="flex-col h-auto text-center items-center justify-center border-2 border-solid border-black rounded-xl">Click to sign-up.{"\n"}</Text>
-                </Pressable>
+  const handleEdit = () => {
+    navigation.navigate("Edit");
+  };
+  if (typeof loggedUser !== "object") {
+    return (
+      <View className="flex-col items-center justify-center bg-lightestBlue h-full ">
+        <View className="bg-sky-200/30 p-4 m-4 rounded-3xl">
+          <Text className="text-center px-1 text-xl">
+            You are currently logged in as a guest who{" "}
+            {over18 ? "is aged 18 or over." : "is not over age 18."} {"\n"}
+          </Text>
+          <Pressable
+            className="bg-mainBlue rounded-3xl w-48 self-center p-1 pt-4 mb-3 px-3"
+            onPress={handleGuestAgeChange}
+          >
+            <Text className="text-center text-white">
+              Change age bracket{"\n"}
+            </Text>
+          </Pressable>
+          <Pressable
+            className="bg-mainBlue rounded-3xl w-48 self-center p-1 pt-4 mb-3 px-3"
+            onPress={handleLogin}
+          >
+            <Text className="text-center text-white">Login{"\n"}</Text>
+          </Pressable>
+          <Pressable
+            className="bg-mainBlue rounded-3xl w-48 self-center p-1 pt-4 mb-3 px-3"
+            onPress={handleGuestSignUp}
+          >
+            <Text className="text-center text-white">Sign-up{"\n"}</Text>
+          </Pressable>
+        </View>
+      </View>
+    );
+  } else if (typeof loggedUser === "object") {
+    return (
+      <ScrollView className="bg-lightestBlue">
+        <View className="flex-row justify-between">
+          <Pressable
+            onPress={handleLogOut}
+            className="overflow-hidden w-10 h-10 border-solid border-2 border-black rounded-lg"
+          >
+            <View className="flex-row justify-start items-center border-2 border-neutral-50 rounded-xl p-1">
+              <Image
+                className="overflow-hidden w-10 h-10 border-solid border-2 border-black rounded-lg"
+                source={{
+                  uri: "https://static.vecteezy.com/system/resources/thumbnails/006/693/054/small/logout-icon-template-black-color-editable-log-out-icon-symbol-flat-illustration-for-graphic-and-web-design-free-vector.jpg",
+                }}
+              />
+              <Text className="font-extrabold">Sign out</Text>
             </View>
-        )
-    } else if (typeof loggedUser === "object") {
-        return (
-            <View className="flex-col items-center justify-center">
-                <View className="w-full h-50 w-50 flex-row justify-between items-between">
-                    <Pressable onPress={handleLogOut} className="overflow-hidden w-10 h-10 border-solid border-2 border-black rounded-lg">
-                    <View className="flex-row justify-start items-center border-2 border-neutral-50 rounded-xl p-1">
-                        <Image className="overflow-hidden w-10 h-10 border-solid border-2 border-black rounded-lg" source={{uri: "https://static.vecteezy.com/system/resources/thumbnails/006/693/054/small/logout-icon-template-black-color-editable-log-out-icon-symbol-flat-illustration-for-graphic-and-web-design-free-vector.jpg"}} />
-                    <Text className="font-extrabold">Sign out</Text>
-                    </View>
-                    </Pressable>
-                    <Pressable onPress={handleEdit} className="bg-white">
-                    <View className="flex-row justify-end items-center border-2 border-neutral-50 rounded-xl p-1">
-                    <Text className="font-extrabold">Edit user</Text>
-                        <Text className="text-3xl bg-white rounded-lg">
-                            ✏️
-                        </Text>
-                    </View>
-                    </Pressable>
-                </View>
-                <View className="flex-row w-full justify-center"><Text className="flex-col items-center justify-center">
-                    You are currently logged in as: {loggedUser.username} {"\n"}
-                </Text>
-
-                </View>
-                <Text>Your avatar link is currently: {"\n"} {loggedUser.avatar ? loggedUser.avatar : "blank"} {"\n"}</Text>
-                <ImageViewer className="justify-start"
-                    placeholderImageSource={loggedUser.avatar ? { uri: loggedUser.avatar } : { uri: "https://www.pngitem.com/pimgs/m/272-2720607_this-icon-for-gender-neutral-user-circle-hd.png" }} logo={false}
-                >
-                </ImageViewer>
-                <Text>
-                    You are currently {loggedUser.over18 ? "aged 18 or over." : "under age 18."}{"\n"}
-                </Text>
-            
-
-
+          </Pressable>
+          <Pressable onPress={handleEdit} className="bg-white">
+            <View className="flex-row justify-end items-center border-2 border-neutral-50 rounded-xl p-1">
+              <Text className="font-extrabold">Edit user</Text>
+              <Text className="text-3xl bg-white rounded-lg">✏️</Text>
             </View>
-        )
-    }
+          </Pressable>
+        </View>
 
+        <View className = 'bg-sky-100/40 m-4 rounded-3xl pt-6'>
+          <View className="flex-row w-full justify-center">
+            <Text className="flex-col  justify-center text-xl px-2 text-center">
+              You are currently logged in as <Text className = 'font-bold'>{loggedUser.username} </Text>{"\n"}
+            </Text>
+          </View>
+          <Text className="text-xl text-center m-2">Your avatar </Text>
+
+          <View className="self-center border border-black rounded-2xl">
+            <ImageViewer
+              placeholderImageSource={
+                loggedUser.avatar
+                  ? { uri: loggedUser.avatar }
+                  : {
+                      uri: "https://www.pngitem.com/pimgs/m/272-2720607_this-icon-for-gender-neutral-user-circle-hd.png",
+                    }
+              }
+              logo={false}
+            ></ImageViewer>
+          </View>
+
+        <Text className="self-center text-xl m-3">
+          You are currently{" "}
+          {loggedUser.over18 ? "aged 18 or over." : "under age 18."}
+          {"\n"}
+        </Text>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
-export default Profile
+export default Profile;
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,137 +1,174 @@
-import { View, TextInput, Text, Button } from "react-native";
+import { View, TextInput, Text, Pressable } from "react-native";
 import { useState } from "react";
 import { getUserByUsername } from "../api";
 import { RadioGroup } from "react-native-radio-buttons-group";
 import { postUser } from "../api";
 
 const SignUp = ({ navigation }) => {
-    const [inputUsername, setInputUsername] = useState("")
-    const [inputPassword, setInputPassword] = useState("")
-    const [inputAvatar, setInputAvatar] = useState("")
-    const [inputOver18, setInputOver18] = useState(false)
-    const [isError, setIsError] = useState({})
-    const [radioButtons, setRadioButtons] = useState([{
-        id: '1',
-        label: '18 or over',
-        value: true
-    }, {
-        id: '2',
-        label: 'under 18',
-        value: false
-    }
-    ])
+  const [inputUsername, setInputUsername] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+  const [inputAvatar, setInputAvatar] = useState("");
+  const [inputOver18, setInputOver18] = useState(false);
+  const [isError, setIsError] = useState({});
+  const [radioButtons, setRadioButtons] = useState([
+    {
+      id: "1",
+      label: "18 or over",
+      value: true,
+    },
+    {
+      id: "2",
+      label: "under 18",
+      value: false,
+    },
+  ]);
 
-    const handleSubmit = () => {
-        const userObject = {}
-        const checkUserPromise = new Promise((resolve, reject) => {
-            getUserByUsername(inputUsername)
-                .then((res) => {
-                    reject("username")
-                })
-                .catch((err) => {
-                    userObject.username = inputUsername
-                    resolve()
-                })
-
+  const handleSubmit = () => {
+    const userObject = {};
+    const checkUserPromise = new Promise((resolve, reject) => {
+      getUserByUsername(inputUsername)
+        .then((res) => {
+          reject("username");
         })
-        checkUserPromise
-            .then(() => {
-
-                if (inputPassword !== "") {
-                    userObject.password = inputPassword
-                }
-                if (inputAvatar !== "") {
-                    userObject.avatar = inputAvatar
-                }
-                let filteredButtons = radioButtons.filter((element) => {
-                    return element.selected === true
-                })
-                if (filteredButtons.length === 0) {
-                    return Promise.reject("age")
-                } else {
-                    if (filteredButtons[0].value) {
-                        userObject.over18 = true;
-                    } else {
-                        userObject.over18 = false;
-                    }
-                    return postUser(userObject)
-                }
-
-            })
-
-            .then((res) => {
-                alert('You successfully created an account.  Please sign in to use our app')
-                navigation.navigate("Login")
-            })
-            .catch((err) => {
-                if (err === "username") {
-                    alert("This username has already been taken.")
-
-                } else if (err === "age") {
-                    alert('You did not select an age bracket.')
-
-                } else if (err.message.slice(-3) === '400') {
-                    alert('You are missing a key field.  Please try again')
-                }
-            })
-    }
-
-
-
-
-
-    function printRadioButtonContents() {
-        let results
-        results = radioButtons.filter((element) => {
-            return element.selected === true
-        })
-        console.log(results[0].value)
-    }
-
-    function onPressRadioButton(radioButtonsArray) {
-        setRadioButtons(radioButtonsArray)
-    }
-
-    const checkUsername = () => {
-        return getUserByUsername(inputUsername)
-            .then((user) => {
-                //username already exists -> sad path as username is unavailable
-                setIsError({ value: true, field: "username" })
-            })
-            .catch((err) => {
-                //username does not exist -> happy path as username is available
-                setIsError({ value: false, field: "" })
-            })
-    }
-
-    const checkAvatar = () => {
-        const regex = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g
-        if (inputAvatar.length > 0) {
-            let result = inputAvatar.match(regex)
-            if (result === null) {
-                setIsError({ value: true, field: "avatar" })
-                alert("Your avatar url should start with 'http://' or 'https://' and end with '.jpg/.png/.gif' ")
-            } else {
-                setIsError({ value: false, field: "" })
-            }
+        .catch((err) => {
+          userObject.username = inputUsername;
+          resolve();
+        });
+    });
+    checkUserPromise
+      .then(() => {
+        if (inputPassword !== "") {
+          userObject.password = inputPassword;
         }
+        if (inputAvatar !== "") {
+          userObject.avatar = inputAvatar;
+        }
+        let filteredButtons = radioButtons.filter((element) => {
+          return element.selected === true;
+        });
+        if (filteredButtons.length === 0) {
+          return Promise.reject("age");
+        } else {
+          if (filteredButtons[0].value) {
+            userObject.over18 = true;
+          } else {
+            userObject.over18 = false;
+          }
+          return postUser(userObject);
+        }
+      })
+
+      .then((res) => {
+        alert(
+          "You successfully created an account.  Please sign in to use our app"
+        );
+        navigation.navigate("Login");
+      })
+      .catch((err) => {
+        if (err === "username") {
+          alert("This username has already been taken.");
+        } else if (err === "age") {
+          alert("You did not select an age bracket.");
+        } else if (err.message.slice(-3) === "400") {
+          alert("You are missing a key field.  Please try again");
+        }
+      });
+  };
+
+  function printRadioButtonContents() {
+    let results;
+    results = radioButtons.filter((element) => {
+      return element.selected === true;
+    });
+    console.log(results[0].value);
+  }
+
+  function onPressRadioButton(radioButtonsArray) {
+    setRadioButtons(radioButtonsArray);
+  }
+
+  const checkUsername = () => {
+    return getUserByUsername(inputUsername)
+      .then((user) => {
+        //username already exists -> sad path as username is unavailable
+        setIsError({ value: true, field: "username" });
+      })
+      .catch((err) => {
+        //username does not exist -> happy path as username is available
+        setIsError({ value: false, field: "" });
+      });
+  };
+
+  const checkAvatar = () => {
+    const regex = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g;
+    if (inputAvatar.length > 0) {
+      let result = inputAvatar.match(regex);
+      if (result === null) {
+        setIsError({ value: true, field: "avatar" });
+        alert(
+          "Your avatar url should start with 'http://' or 'https://' and end with '.jpg/.png/.gif' "
+        );
+      } else {
+        setIsError({ value: false, field: "" });
+      }
     }
-    return (
-        <View>
-            <Text>Please enter your details in the fields below then click submit.</Text>
-            <Text>Username</Text>
-            <TextInput className={isError.field === "username" ? "border-2 border-solid border-red-200" : "border-2 border-solid border-black-200"} value={inputUsername} onEndEditing={checkUsername} onChangeText={(inputText) => { setInputUsername(inputText) }} placeholder="e.g. TestUser1..."></TextInput>
-            <Text>Password</Text>
-            <TextInput placeholder="...make me strong" className={"border-2 border-solid border-black-200"} onChangeText={(inputText) => { setInputPassword(inputText) }} value={inputPassword}></TextInput>
-            <Text>Avatar URL</Text>
-            <TextInput placeholder="https://examplewebsite.com/my_avatar_image.jpg" className={isError.field === "avatar" ? "border-2 border-solid border-red-200" : "border-2 border-solid border-black-200"} onChangeText={(inputText) => { setInputAvatar(inputText) }} value={inputAvatar} onEndEditing={checkAvatar}></TextInput>
-            <Text>Age</Text>
-            <RadioGroup radioButtons={radioButtons} onPress={onPressRadioButton} />
-            <Button title="Submit" onPress={handleSubmit} ></Button>
+  };
+  return (
+    <View className="bg-lightestBlue h-full">
+      <View className="bg-sky-200/30 m-4 p-4 rounded-3xl">
+        <Text>Username</Text>
+        <TextInput
+          className={
+            isError.field === "username"
+              ? "mb-4 px-2 border-2 border-solid border-red-200 rounded-md bg-sky-100/20"
+              : "mb-4 px-2 border-2 border-solid border-black-200 rounded-md bg-sky-100/20"
+          }
+          value={inputUsername}
+          onEndEditing={checkUsername}
+          onChangeText={(inputText) => {
+            setInputUsername(inputText);
+          }}
+          placeholder="e.g. TestUser1..."
+        ></TextInput>
+        <Text>Password</Text>
+        <TextInput
+          placeholder="...make me strong"
+          className="border-2 border-solid border-black-200 px-2 mb-4 bg-sky-100/20 rounded-md"
+          onChangeText={(inputText) => {
+            setInputPassword(inputText);
+          }}
+          value={inputPassword}
+        ></TextInput>
+        <Text>Avatar URL</Text>
+        <TextInput
+          placeholder="https://examplewebsite.com/my_avatar_image.jpg"
+          className={
+            isError.field === "avatar"
+              ? "border-2 border-solid border-red-200 px-2 mb-4 bg-sky-100/20 rounded-md"
+              : "border-2 border-solid border-black-200 px-2 mb-4 bg-sky-100/20 rounded-md"
+          }
+          onChangeText={(inputText) => {
+            setInputAvatar(inputText);
+          }}
+          value={inputAvatar}
+          onEndEditing={checkAvatar}
+        ></TextInput>
+
+        <View className = 'bg-sky-100/20 w-2/5 self-center rounded-lg p-3'>
+          <Text className="text-center text-xl p-2">Age</Text>
+          <RadioGroup
+            radioButtons={radioButtons}
+            onPress={onPressRadioButton}
+          />
+          <Pressable className = 'bg-darkBlue rounded-xl w-3/5 self-center mt-4 p-2' title="Submit" onPress={handleSubmit}>
+            <Text className = 'text-center text-white'>Submit</Text>
+          </Pressable>
         </View>
-    )
-}
 
 
+      </View>
+    </View>
+  );
+};
 
 export default SignUp;
