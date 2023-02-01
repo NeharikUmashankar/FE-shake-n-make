d72cfad18765e348279a552a6d060803264f1143
Neharik Umashankar <neharik.umashankar@kpmg.co.uk>
2023-02-01 11:42:52 +0000

    Finished some more styling

--- components/Header.js
+++ components/Header.js
@@ -1,47 +1,64 @@
-import { View,StyleSheet } from "react-native"
-import ImageViewer from "./ImageViewer"
-import { useContext } from "react"
-import { UserContext } from "./UserContext"
-import { AdultContext } from "./AdultContext"
-import Pressable from "react-native/Libraries/Components/Pressable/Pressable"
-import {useNavigation} from '@react-navigation/native'
-import { Text } from "react-native"
-const Header = ({title}) => {
-    const {over18,setOver18} = useContext(AdultContext)
-    const navigation=useNavigation()
-    const {loggedUser,setLoggedUser} = useContext(UserContext)
-    const handleGuestOnPress = () => {
-        navigation.navigate("Sign up")
-    }
-    const handleUserOnPress = () => {
-        navigation.navigate("Favourites")
-    }
+import { View, StyleSheet } from "react-native";
+import ImageViewer from "./ImageViewer";
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+import { AdultContext } from "./AdultContext";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import { useNavigation } from "@react-navigation/native";
+import { Text } from "react-native";
+const Header = ({ title }) => {
+  const { over18, setOver18 } = useContext(AdultContext);
+  const navigation = useNavigation();
+  const { loggedUser, setLoggedUser } = useContext(UserContext);
+  const handleGuestOnPress = () => {
+    navigation.navigate("Sign up");
+  };
+  const handleUserOnPress = () => {
+    navigation.navigate("Favourites");
+  };
 
-    return (
-        <View className="flex-column w-full p-0 h-full">
-        <View className="flex-row h-fit justify-around w-full mr-50 pr-50">
-            <ImageViewer className="justify-start"
-                placeholderImageSource={{uri: "https://freesvg.org/img/bar-15.png"}} logo={true}
-                >
-            </ImageViewer>
-            <Text>{title}</Text>
-            <Pressable onPress={typeof loggedUser !== "object" ? handleGuestOnPress : handleUserOnPress }>
-            <ImageViewer
-            placeholderImageSource={typeof loggedUser!=="object" ? {uri: "https://images.macrumors.com/t/n4CqVR2eujJL-GkUPhv1oao_PmI=/1600x/article-new/2019/04/guest-user-250x250.jpg"}:(loggedUser.avatar ? {uri: loggedUser.avatar} : {uri: "https://www.pngitem.com/pimgs/m/272-2720607_this-icon-for-gender-neutral-user-circle-hd.png"})} logo={true}
-            ></ImageViewer>
-            </Pressable>
-        </View>
-        
-        </View>
-    )
-}
+  return (
+    <View className="w-full">
+      <View className="flex-row h-fit justify-around w-96 mb-3 p-1">
+        <ImageViewer
+          className=""
+          placeholderImageSource={{ uri: "https://freesvg.org/img/bar-15.png" }}
+          logo={true}
+        ></ImageViewer>
+        <Text className = 'text-xl w-2/5 text-center'>{title}</Text>
+        <Pressable
+          onPress={
+            typeof loggedUser !== "object"
+              ? handleGuestOnPress
+              : handleUserOnPress
+          }
+        >
+          <ImageViewer
+            placeholderImageSource={
+              typeof loggedUser !== "object"
+                ? {
+                    uri: "https://images.macrumors.com/t/n4CqVR2eujJL-GkUPhv1oao_PmI=/1600x/article-new/2019/04/guest-user-250x250.jpg",
+                  }
+                : loggedUser.avatar
+                ? { uri: loggedUser.avatar }
+                : {
+                    uri: "https://www.pngitem.com/pimgs/m/272-2720607_this-icon-for-gender-neutral-user-circle-hd.png",
+                  }
+            }
+            logo={true}
+          ></ImageViewer>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
 
-const styles= StyleSheet.create({
-    container: {
-        flex: 1,
-        height: 60,
-        width: 50,
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 60,
+    width: 50,
+  },
+});
 
-export default Header
+export default Header;
--- routes/Stack.js
+++ routes/Stack.js
@@ -14,59 +14,118 @@ import Login from "../components/Login";
 import SignUp from "../components/SignUp";
 import ViewFavourites from "../components/ViewFavourites";
 import Header from "../components/Header";
+import { white } from "tailwindcss/colors";
 const Stack = createNativeStackNavigator();
 // export default createAppContainer(HomeStack);
-export default MyStack = ({ name, navigation}) => {
-    // console.log(name,"<<insideStack")
-    return (
-        <Stack.Navigator initialRouteName={name}>
-            <Stack.Screen name="Welcome" component={LandingScreen}
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="Welcome"/> }}
-            />
-            <Stack.Screen name="Home" component={HomeScreen}
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="Home"/> }}
-            />
-            <Stack.Screen name="Virgin?" component={NonAL}
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="Non-alcoholic cocktails"/> }}
-            />
-            <Stack.Screen name="Random cocktail" component={RandomCocktails}
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="Random cocktail"/> }}
-            />
-            <Stack.Screen name="Alphabet List"
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="Alphabet List"/> }}
-            >
-                {(props) => <Alphabet {...props} />}
-            </Stack.Screen>
-            <Stack.Screen name="Cocktails by Alphabet"
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="Cocktails by Letter"/> }}
-            >
-                {(props) => <CocktailsAZ {...props} />}
-            </Stack.Screen>
-            <Stack.Screen name="Cocktail"
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="Cocktail"/> }}
-            >
-                {(props) => <Cocktail {...props} />}
-            </Stack.Screen>
-            <Stack.Screen name="Filter Input" component={FilterInput}
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header /> }}
-            />
-            <Stack.Screen name="Filtered Cocktails" component={FilteredCocktails}
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="Filtered cocktails"/> }}
-            />
-            <Stack.Screen name="Login" component={Login}
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="Login"/> }}
-            />
-            <Stack.Screen name="Sign up" component={SignUp}
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="Sign up"/> }}
-            />
-            <Stack.Screen name="Favourites" component={ViewFavourites}
-                options={{ headerBackVisible: false,headerTitle: (props) => <Header title="My favourites" /> }}
-            />
-
-
-        </Stack.Navigator>
-    )
-}
+export default MyStack = ({ name, navigation }) => {
+  // console.log(name,"<<insideStack")
+  return (
+    <Stack.Navigator initialRouteName={name}>
+      <Stack.Screen
+        name="Welcome"
+        component={LandingScreen}
+        options={{
+          headerStyle : {
+            backgroundColor: '#ffffff'
+          },
+          headerBackVisible: false,
+          headerTitle: (props) => <Header className="" title="Welcome" />,
+        }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header title="Home" />,
+        }}
+      />
+      <Stack.Screen
+        name="Virgin?"
+        component={NonAL}
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header title="Non-alcoholic cocktails" />,
+        }}
+      />
+      <Stack.Screen
+        name="Random cocktail"
+        component={RandomCocktails}
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header title="Random cocktail" />,
+        }}
+      />
+      <Stack.Screen
+        name="Alphabet List"
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header title="Alphabet List" />,
+        }}
+      >
+        {(props) => <Alphabet {...props} />}
+      </Stack.Screen>
+      <Stack.Screen
+        name="Cocktails by Alphabet"
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header title="Cocktails by Letter" />,
+        }}
+      >
+        {(props) => <CocktailsAZ {...props} />}
+      </Stack.Screen>
+      <Stack.Screen
+        name="Cocktail"
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header title="Cocktail" />,
+        }}
+      >
+        {(props) => <Cocktail {...props} />}
+      </Stack.Screen>
+      <Stack.Screen
+        name="Filter Input"
+        component={FilterInput}
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header />,
+        }}
+      />
+      <Stack.Screen
+        name="Filtered Cocktails"
+        component={FilteredCocktails}
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header title="Filtered cocktails" />,
+        }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header title="Login" />,
+        }}
+      />
+      <Stack.Screen
+        name="Sign up"
+        component={SignUp}
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header title="Sign up" />,
+        }}
+      />
+      <Stack.Screen
+        name="Favourites"
+        component={ViewFavourites}
+        options={{
+          headerBackVisible: false,
+          headerTitle: (props) => <Header title="My favourites" />,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 // const screens = {
 //   Welcome: {
@@ -111,5 +170,3 @@ export default MyStack = ({ name, navigation}) => {
 //     screen: SignUp,
 //   }
 // };
-
-
