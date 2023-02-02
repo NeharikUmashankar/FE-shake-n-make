abeeed9da107c6832d3e7d91988ca579f5590bb8
Qasim Hussain <qasim.hussain1@kpmg.co.uk>
2023-02-02 15:35:59 +0000

    Error messages

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import { registerRootComponent } from 'expo';
+import { LogBox } from "react-native";
 
 import App from './App';
+
+
+
+LogBox.ignoreAllLogs(); // Ignore log notification by messageLogBox.ignoreAllLogs();//Ignore all log notifications
+
+
+
+console.disableYellowBox = true;
+
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
